47f178284fed2584af0e233e90ac830c653f2a94
bbenligiray <bbenligiray@gmail.com>
2023-03-14 16:59:52 +0300

    Remove old scripts

--- scripts/read-data-feed.js
+++ scripts/read-data-feed.js
@@ -7,15 +7,9 @@ async function main() {
     DataFeedReaderExample.abi,
     hre.ethers.provider
   );
-  const dataFeedId = process.env.DATA_FEED_ID;
-  if (!dataFeedId) {
-    throw new Error('Data feed ID not defined');
-  }
-  const dataFeedValue = await dataFeedReaderExample.readDataFeedValueWithId(dataFeedId);
+  const dataFeed = await dataFeedReaderExample.readDataFeed();
   console.log(
-    `DataFeedReaderExample at ${
-      DataFeedReaderExample.address
-    } read data feed value with ID ${dataFeedId} as \n  value: ${dataFeedValue.toString()}`
+    `DataFeedReaderExample at ${DataFeedReaderExample.address} read the data feed as:\n ${dataFeed.toString()}`
   );
 }
 
