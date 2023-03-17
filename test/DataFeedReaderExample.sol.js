513d05359ad203a31784e37bb6fc0fcd4c156504
bbenligiray <bbenligiray@gmail.com>
2023-03-17 16:46:44 +0300

    Extend tests

--- test/DataFeedReaderExample.sol.js
+++ test/DataFeedReaderExample.sol.js
@@ -16,20 +16,70 @@ describe('DataFeedReaderExample', function () {
     const dataFeedReaderExampleFactory = await ethers.getContractFactory('DataFeedReaderExample', roles.owner);
     const dataFeedReaderExample = await dataFeedReaderExampleFactory.deploy(mockProxy.address);
     return {
+      roles,
       mockProxy,
       dataFeedReaderExample,
     };
   }
+  describe('constructor', function () {
+    it('constructs', async function () {
+      const { roles, mockProxy, dataFeedReaderExample } = await helpers.loadFixture(deploy);
+      expect(await dataFeedReaderExample.owner()).to.equal(roles.owner.address);
+      expect(await dataFeedReaderExample.proxy()).to.equal(mockProxy.address);
+    });
+  });
+
+  describe('setProxy', function () {
+    context('Sender is the owner', function () {
+      it('sets proxy', async function () {
+        const { roles, dataFeedReaderExample } = await helpers.loadFixture(deploy);
+        const newProxyAddress = ethers.Wallet.createRandom().address;
+        await dataFeedReaderExample.connect(roles.owner).setProxy(newProxyAddress);
+        expect(await dataFeedReaderExample.proxy()).to.equal(newProxyAddress);
+      });
+    });
+    context('Sender is not the owner', function () {
+      it('reverts', async function () {
+        const { roles, dataFeedReaderExample } = await helpers.loadFixture(deploy);
+        const newProxyAddress = ethers.Wallet.createRandom().address;
+        await expect(dataFeedReaderExample.connect(roles.randomPerson).setProxy(newProxyAddress)).to.be.revertedWith(
+          'Ownable: caller is not the owner'
+        );
+      });
+    });
+  });
 
   describe('readDataFeed', function () {
-    it('reads data feed', async function () {
-      const { mockProxy, dataFeedReaderExample } = await helpers.loadFixture(deploy);
-      const dataFeedValue = 123456;
-      const dataFeedTimestamp = (await ethers.provider.getBlock()).timestamp;
-      await mockProxy.mock(dataFeedValue, dataFeedTimestamp);
-      const dataFeed = await dataFeedReaderExample.readDataFeed();
-      expect(dataFeed.value).to.equal(dataFeedValue);
-      expect(dataFeed.timestamp).to.equal(dataFeedTimestamp);
+    context('Value is positive', function () {
+      context('Timestamp is not older than a day', function () {
+        it('reads data feed', async function () {
+          const { mockProxy, dataFeedReaderExample } = await helpers.loadFixture(deploy);
+          const dataFeedValue = 123456;
+          const dataFeedTimestamp = (await ethers.provider.getBlock()).timestamp;
+          await mockProxy.mock(dataFeedValue, dataFeedTimestamp);
+          const dataFeed = await dataFeedReaderExample.readDataFeed();
+          expect(dataFeed.value).to.equal(dataFeedValue);
+          expect(dataFeed.timestamp).to.equal(dataFeedTimestamp);
+        });
+      });
+      context('Timestamp is older than a day', function () {
+        it('reverts', async function () {
+          const { mockProxy, dataFeedReaderExample } = await helpers.loadFixture(deploy);
+          const dataFeedValue = 123456;
+          const dataFeedTimestamp = (await ethers.provider.getBlock()).timestamp - 24 * 60 * 60;
+          await mockProxy.mock(dataFeedValue, dataFeedTimestamp);
+          await expect(dataFeedReaderExample.readDataFeed()).to.be.revertedWith('Timestamp older than one day');
+        });
+      });
+    });
+    context('Value is not positive', function () {
+      it('reverts', async function () {
+        const { mockProxy, dataFeedReaderExample } = await helpers.loadFixture(deploy);
+        const dataFeedValue = -123456;
+        const dataFeedTimestamp = (await ethers.provider.getBlock()).timestamp;
+        await mockProxy.mock(dataFeedValue, dataFeedTimestamp);
+        await expect(dataFeedReaderExample.readDataFeed()).to.be.revertedWith('Value not positive');
+      });
     });
   });
 });
