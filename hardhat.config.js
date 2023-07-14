adc30410cfbbf268149e013a5c1bbc79d29518dd
bbenligiray <bbenligiray@gmail.com>
2023-07-14 15:32:19 +0300

    Bump dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,10 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('hardhat-deploy');
-const api3Chains = require('@api3/chains');
+const { hardhatConfig } = require('@api3/chains');
 require('dotenv').config();
 
-const networks = Object.entries(api3Chains.hardhatConfigNetworks()).reduce((networksWithMnemonic, networkEntry) => {
-  const chainAlias = networkEntry[0];
-  const network = networkEntry[1];
-  networksWithMnemonic[chainAlias] = {
-    ...network,
-    accounts: { mnemonic: process.env.MNEMONIC ? process.env.MNEMONIC : '' },
-  };
-  return networksWithMnemonic;
-}, {});
-
 module.exports = {
-  networks,
+  networks: hardhatConfig.networks(),
   solidity: {
     version: '0.8.17',
   },
