622124774a6d64d09575582097fd9d9336c92877
Santiago Aceñolaza <santiago@api3.org>
2024-05-03 16:19:49 -0300

    yarn prettier

--- scripts/print-supported-chains.js
+++ scripts/print-supported-chains.js
@@ -1,11 +1,16 @@
-const { getChains, api3Chains } = require("@api3/dapi-management");
+const { getChains, api3Chains } = require('@api3/dapi-management');
 
 async function main() {
   const supportedChains = getChains()
-    .filter(chain => ['active', 'deprecated'].includes(chain.stage))
-    .map(chain => {
+    .filter((chain) => ['active', 'deprecated'].includes(chain.stage))
+    .map((chain) => {
       const { alias, testnet } = api3Chains.CHAINS.find(({ id }) => id === chain.id);
-      return { id: Number(chain.id), name: alias, type: testnet ? 'testnet' : 'mainnet', marketLink: `https://market.api3.org/${alias}` };
+      return {
+        id: Number(chain.id),
+        name: alias,
+        type: testnet ? 'testnet' : 'mainnet',
+        marketLink: `https://market.api3.org/${alias}`,
+      };
     })
     .sort((a, b) => a.name.localeCompare(b.name));
 
